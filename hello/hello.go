2b68dd7fd1a4d6dbebdd718311356bc9823192a8
OmegaWei1984 <omegawei1984@gmail.com>
2023-03-04 23:44:02 +0800

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
     "rsc.io/quote"
     "example.com/greetings"
 )
@@ -9,7 +10,12 @@ import (
 func main() {
     fmt.Println("Hello, World!")
     fmt.Println(quote.Go())
-    msg := greetings.Hello("golang")
+    msg, _ := greetings.Hello("golang")
     fmt.Println(msg)
+    msg2, err := greetings.Hello("")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(msg2)
 }
 
