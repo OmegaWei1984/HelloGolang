d99c0e73b95baf15821a7bc41a8af41437cfa951
OmegaWei1984 <omegawei1984@gmail.com>
2023-03-05 13:23:48 +0800

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "rsc.io/quote"
-    "example.com/greetings"
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+	"rsc.io/quote"
 )
 
-
 func testQuote() {
-    fmt.Println(quote.Go())
+	fmt.Println(quote.Go())
 }
 
 func testHello() {
-    msg, _ := greetings.Hello("golang")
-    fmt.Println(msg)
+	msg, _ := greetings.Hello("golang")
+	fmt.Println(msg)
+}
+
+func testHellos() {
+	names := []string{"foo", "bar", "baz"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
 }
 
 func testHelloHanleError() {
-    msg2, err := greetings.Hello("")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(msg2)
+	msg2, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msg2)
 }
 
 func main() {
-    testHello()    
+	testHello()
+	testHellos()
 }
-
